5a944ec9b31e467819b76c3171df3195ca552111
MasonClark24 <mclark24@mail.umw.edu>
2024-07-25 15:43:12 -0400

    include lanczos progress, though there isn't any

--- functions.py
+++ functions.py
@@ -3,6 +3,8 @@ from itertools import product
 import numpy as np
 from scipy.linalg import eigh
 import math
+import numpy.linalg as la
+from scipy.linalg import eigh_tridiagonal, inv
 from wigners import wigner_3j
 
 
@@ -153,7 +155,6 @@ def eigenvector_and_eigenvalues(hamiltonian, Kt, eigenvectors_I_want):
     Return the eigenvalues / vectors for the eigenvectors_I_want, given the hamiltonian and Kt
     """
 
-
     eigenvalues, eigenvectors = compute_eigenvalues_and_vectors_eigh(hamiltonian, Kt)
     eigenvalues += (2 * Kt ** 2 / 3)  # correction factor. See sakamoto
 
@@ -173,8 +174,6 @@ def eigenvector_and_eigenvalues(hamiltonian, Kt, eigenvectors_I_want):
     return np.array(T2_eigenvectors_in_BO), np.array(T2_BO_eigenvalues)
 
 
-
-
 def AMCOs(Jn, Jpnp, Kn, Kpnp, Mn, Mpnp, K, Q, S):
     """
     Given the J, K, M, for the row/col and KQS, returns the amco value
@@ -185,6 +184,7 @@ def AMCOs(Jn, Jpnp, Kn, Kpnp, Mn, Mpnp, K, Q, S):
     line1 = wigner_3j(Jn, Jpnp, K, -Mn, Mpnp, Q) * wigner_3j(Jn, Jpnp, K, -Kn, Kpnp, S)
     A = line0 * line1
     return A
+
 
 def full_amco(J_0, K, Q, S, vector_parts):
     """
@@ -229,7 +229,7 @@ def full_amco(J_0, K, Q, S, vector_parts):
     how_many_amco_copies = size_of_electronic_section // num_unique_indices
     amco_list = np.tile(amco_list, (how_many_amco_copies, how_many_amco_copies))
 
-    return amco_list    # makes an amco matrix the size of the density matrix, but only had to calculate a portion of it
+    return amco_list  # makes an amco matrix the size of the density matrix, but only had to calculate a portion of it
 
 
 def psi_t_function(psi_0, constant, t, num_psis):
@@ -293,8 +293,6 @@ def psi(T2_eigenvectors_in_BO, T2_BO_eigenvalues, states, B, J_0, M_0):
     return psi, np.array(psi_t_vector_parts)
 
 
-
-
 def a(elec_state, J, Jin, M, K):
     mux, muy, muz = 0, 0, 0
 
@@ -323,22 +321,217 @@ def a(elec_state, J, Jin, M, K):
 
     return A
 
+
 def find_diagonal_indices(n):
-    flattened_indices = np.arange(n**2)
-    diagonal_indices = flattened_indices[::n+1]
+    flattened_indices = np.arange(n ** 2)
+    diagonal_indices = flattened_indices[::n + 1]
     return diagonal_indices
+
+def hamil(q, Kt, states=None):
+    # set up hamiltonian
+
+    if states is None:
+        states = get_possible_states(q)
+
+    H = np.array([[matrix_elem(state1, state2).real for state2 in states] for state1 in states])
+    mask = ~np.eye(H.shape[0], dtype=bool)
+    H[mask] *= Kt
+
+    return H
+
+
+def Lanczos(H, vg, num_vectors=None):
+    """
+    LANCZOS ALGORITHM
+
+    :param H: Original Hamil
+    :param vg: Initial Vector from Symmetry Consideration
+    :param num_vectors: Can continue until the algorithm breaks or select a finite number of vectors
+    :return: Tri-diagonal Matrix of order (num_vectors) with (ideally) the eigenvalues of that symmetry
+    """
+
+    if num_vectors is None:
+        num_vectors = len(vg)
+
+    P = np.zeros((H.shape[0], num_vectors), dtype=complex)  # Transformation matrix
+    T = np.zeros((num_vectors, num_vectors), dtype=complex)  # New Hamil
+    P[:, 0] = vg / la.norm(vg)  # Normalizes initial vector
+
+    # Performs the first iteration step of the Lanczos algorithm
+    w = np.dot(H, P[:, 0])
+    a = np.dot(np.conj(w), P[:, 0])
+    w = w - a * P[:, 0]
+    T[0, 0] = a
+
+    # Continue until number of desired vectors/iterations is complete
+    for j in range(1, num_vectors):
+
+        b = la.norm(w)
+
+        # if b is near zero, the algorithm is complete and stops early
+        if b < 1e-4:
+            # print("Break Lanczos algorithm due to small b.")
+            # print(f"b={b}, b[prior]={T[j - 2, j - 1]}")
+            print("Break Lanczos algorithm due to small b.", j, "/", len(vg))
+            break
+
+        P[:, j] = w / b
+
+        # Orthogonalize the newly generated vector for extra precision
+        for k in range(j):
+            P[:, j] -= np.dot(np.conj(P[:, j]), P[:, k]) * P[:, k]
+
+        w = np.dot(H, P[:, j])
+        a = np.dot(np.conj(w), P[:, j])
+        w = w - a * P[:, j] - b * P[:, j - 1]
+
+        # Creates tridiagonal matrix T using a and b values
+        T[j, j] = a
+        T[j - 1, j] = b
+        T[j, j - 1] = np.conj(b)
+
+    # works!!!
+    X = np.real(np.dot(np.conj(P.T), np.dot(H, P)).round(7))
+
+
+    return T, P
+
+
+def Lanczos_hamil(q, Kt, H=None, num_vectors=None):
+    if H is None:
+        H = hamil(q, Kt)
+
+    # Choose an initial vector
+    v = np.zeros(H.shape[0])
+    v[0] = 0
+    v[1] = -1 / np.sqrt(2)
+    v[2] = 1 / np.sqrt(2)
+
+    T, P = Lanczos(H, v, num_vectors)
+    diag = np.real(np.diag(T))
+    sub_diag = np.real(np.diag(T, -1))
+
+    eigenvalues = eigh_tridiagonal(diag, sub_diag, eigvals_only=True)
+    """    # has a lowest eignevalue of 0, which is not valid, so don't use those
+    eigenvalues = eigh(T, eigvals_only=True)
+    eigenvalues = eigenvalues[eigenvalues != 0]"""
+
+    return eigenvalues
+
+
+def states_in_chunk(state1, q):
+    """
+    There are four chunks in this hamiltonian, and each is found by determining which states
+    interact with the basic one.
+
+    First chunk is what interacts with 000X, next is 000Y, 3rd is 000Z, and the last is what is left over
+
+
+    :param state1: Ex. |000| |x>
+    :param q: Number of quanta
+    :return: List of states for that chunk
+    """
+
+    states = get_possible_states(q)
+    states_in_this_chunk = [state1]
+
+    states.remove(state1)
+
+    for state1 in states_in_this_chunk:
+        for state2 in states:
+            if np.real(matrix_elem(state1, state2)) > 0:
+                states_in_this_chunk.append(state2)
+                states.remove(state2)
+
+
+    return states_in_this_chunk
+
+def states_in_chunk(state1, q):
+
+    states = get_possible_states(q)
+    for state in [[0, 0, 0, 'X'], [0, 0, 1, 'Y'], [0, 1, 0, 'Z'], [1, 0, 1, 'Z'], [0, 0, 2, 'X'], [0, 2, 0, 'X']]:
+        for n in states:
+            if np.real(matrix_elem(state, n)) > 0:
+                #print(state, n)
+                pass
+
+    states_in_this_chunk = [state1]
+
+    # already contains the one 000X/y/z state, so continue from q=1
+    for i in range(1, q+1):
+        state_list = distribute_balls(i)
+
+        electronic = ["X", "Y", "Z"]
+
+        # provide all states
+        for vib in electronic:
+            for item in state_list:
+                state = item + [vib]
+
+                for chunk_state in states_in_this_chunk:
+                    if state in states_in_this_chunk:
+                        continue
+
+                    if np.real(matrix_elem(chunk_state, state)) > 0:
+                        states_in_this_chunk.append(state)
+
+    return states_in_this_chunk
+
+def states_in_chunk3(state1, q):
+
+
+    states_in_this_chunk = [state1]
+
+    # already contains the one 000X/y/z state, so continue from q=1
+    S = []
+    for i in range(1, q + 1):
+        state_list = distribute_balls(i)
+
+        electronic = ["X", "Y", "Z"]
+
+        # provide all states
+        for vib in electronic:
+            for item in state_list:
+                S.append(item + [vib])
+
+
+    for state in states_in_this_chunk:
+        for state2 in S:
+            if state2 in states_in_this_chunk:
+                continue
+            if np.real(matrix_elem(state, state2)) > 0:
+                states_in_this_chunk.append(state2)
+
+
+    return states_in_this_chunk
+
+def distribute_balls(q):
+    """
+    :param q: Distribute the quanta into three buckets
+    :return: combinations of these
+    """
+
+    combinations = []
+    for distribution in product(range(q + 1), repeat=3):
+        if sum(distribution) == q:
+            combinations.append(list(distribution))
+
+    # combinations.append(combinations.pop(0))    # optional... moves first item to end
+
+    return combinations
+
 
 def E_jkm(B, J):
     return B * J * (J + 1)
 
 
-time_conversion = 1 / 41.341378     # AU =  41.3 * fs
+time_conversion = 1 / 41.341378  # AU =  41.3 * fs
 M_0 = 0
 # B = 1.71270 / 4.134e16  # AU
-B = 2.603010273176e-7     #1.71270 Ghz to eV then to AU
+B = 2.603010273176e-7  #1.71270 Ghz to eV then to AU
 
 Kt = 0.1
-q = 10    # any less than 2 gets wonky
+q = 10  # any less than 2 gets wonky
 
 print(f"-------- q = {q} --------")
 
